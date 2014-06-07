22d594b6baf1159813b3c67691f5e0fda2b89df2
shingo suzuki <s_suzuking@icloud.com>
2014-06-08 05:29:05 +0900

    error fix

--- lib/fluent/plugin/in_redis_slowlog.rb
+++ lib/fluent/plugin/in_redis_slowlog.rb
@@ -5,7 +5,7 @@ class Fluent::Redis_SlowlogInput < Fluent::Input
   config_param :host,     :string,  :default => nil
   config_param :port,     :integer, :default => 6379
   config_param :logsize,  :integer,  :default => 128
-  config_param :interval, :integer,  :default => 128
+  config_param :interval, :integer,  :default => 10
 
 
   def initialize
@@ -19,6 +19,12 @@ class Fluent::Redis_SlowlogInput < Fluent::Input
       :host => @host, 
       :port => @port
     )
+    pong = @redis.ping
+    unless pong == 'PONG'
+        raise "fluent-plugin-redis-slowlog: cannot connect redis"
+    end
+    @log_id = 0
+    @get_interval = @interval
   end
 
   def start
@@ -33,24 +39,24 @@ class Fluent::Redis_SlowlogInput < Fluent::Input
   private
   def watch
     while true
-      sleep interval
-      id = output( id )
+      sleep @get_interval
+      @log_id = output( @log_id )
     end
   end
 
   def output( last_id = 0) 
-    id = 0
     slow_logs = []
     slow_logs = @redis.slowlog('get', logsize)
 
+    log_id = slow_logs[0][0]
     slow_logs.each do |log|
-      if log[0] <= last_id
+      unless log[0] > last_id
         break
       end
-      log_hash = { id: log[0], timestamp: log[1], exec_time: log[2], command: log[3] }
-      Fluent::Engine.emit(tag, Fluent::Engine.now, log_hash)
-      id = log_hash[:id]
+      log_hash = { id: log[0], timestamp: log[1] exec_time: log[2], command: log[3] }
+      Fluent::Engine.emit(tag, Time.now.to_i, log_hash)
     end
+    return log_id
   end
 end
 
